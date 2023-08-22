d9c85ce3732bc160f85fc85fb8dea025593edd30
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-08-22 12:41:24 -0300

    feat: create survey component

--- src/app/components/survey-administration/survey-administration.component.ts
+++ src/app/components/survey-administration/survey-administration.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SurveyService } from 'src/app/services/survey.service';
+import { Survey } from 'src/app/types/Survey';
 
 @Component({
   selector: 'app-survey-administration',
   templateUrl: './survey-administration.component.html',
   styleUrls: ['./survey-administration.component.css']
 })
-export class SurveyAdministrationComponent {
+export class SurveyAdministrationComponent implements OnInit {
+  surveys: Survey[] = [];
 
+  constructor(private surveyService: SurveyService) {
+  }
+
+  ngOnInit(): void {
+    this.setSurveys()
+  }
+
+  setSurveys() : void {
+    this.surveyService.getSurveys().subscribe((surveys) => this.surveys = surveys);
+  }
 }
