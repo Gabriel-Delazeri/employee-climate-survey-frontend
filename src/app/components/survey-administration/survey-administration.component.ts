56a16814ac5865ad5ca9277808696d0dd23114d4
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-08-22 12:20:38 -0300

    feat: base components

---
+++ src/app/components/survey-administration/survey-administration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-administration',
+  templateUrl: './survey-administration.component.html',
+  styleUrls: ['./survey-administration.component.css']
+})
+export class SurveyAdministrationComponent {
+
+}
