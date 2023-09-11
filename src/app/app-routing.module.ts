d53523c832e7fa4a3ad7b8730fe8eb5f2d718a31
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-09-11 09:41:40 -0300

    feat: delete unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { SurveyAdministrationComponent } from './components/survey-administration/survey-administration.component';
-import { SurveyPageAdminstrationComponent } from './components/survey-page-adminstration/survey-page-adminstration.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'admin/surveys', component: SurveyAdministrationComponent},
-  { path: 'admin/surveys/:surveyId', component: SurveyPageAdminstrationComponent}
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { SurveyAdministrationComponent } from './components/survey-administration/survey-administration.component';
 import { LucideAngularModule, Eye, Edit, Trash, Plus, X } from 'lucide-angular';
-import { SurveyPageAdminstrationComponent } from './components/survey-page-adminstration/survey-page-adminstration.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { AddQuestionModalComponent } from './components/add-question-modal/add-question-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SurveyAdministrationComponent,
-    SurveyPageAdminstrationComponent,
-    AddQuestionModalComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
